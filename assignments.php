b6e84ba5c2b89a22fb2d1c7551470e46970f6257
Hart-Brandon <brandonthart.gs@gmail.com>
2016-05-04 12:42:00 -0600

    test commit

--- assignments.php
+++ assignments.php
@@ -2,6 +2,6 @@
 <br><br>
 <h2 class="h_tab">Assignment Links</h2>
 <ol class="p_tab">
-    <li><a href="assign/hello_world.html" title="Assignment 1-Hello World">OpenShift RHCloud Hello World</li>
+    <li><a href="assign/hello_world.html" title="Assignment-Hello World">OpenShift RHCloud Hello World</li>
 </ol>
 <?php include ('view/footer.php') ?>
