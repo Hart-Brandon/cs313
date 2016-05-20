26bf465fd0ff9dd2b8597bff3eef80a84e845610
Hart-Brandon <brandonthart.gs@gmail.com>
2016-05-20 12:00:44 -0600

    Change status on r_home page

--- r_home.php
+++ r_home.php
@@ -5,11 +5,14 @@
             <?php include ('r_view/top_nav.php') ?>
             <h1>Recipe Repository Home</h1>
             <h2>Future Site Functionality:</h2>
+            
             <p>IN PROGRESS</p>
-            <h4>View Current Recipes</h4>
-            <p>ON HOLD</p>
             <h4>Add New Recipes</h4>
             <h4>Edit Personal Recipes</h4>
+            
+            <p>ON HOLD</p>
             <h4>User Login</h4>
+            
             <p>COMPLETED</p>
+            <h4>View Current Recipes</h4>
 <?php include ('r_view/footer.php') ?>
