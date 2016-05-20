28f01eb38ed96072378a1c15cc4c5c0757f5515f
Hart-Brandon <brandonthart.gs@gmail.com>
2016-05-20 11:49:21 -0600

    Database Read-Only Update

--- model/database.php
+++ model/database.php
@@ -3,14 +3,38 @@
     /**
      * Production connection settings
      */
-//    $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
-//    $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
-//    $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
-//    $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+    $dbHost = "";
+    $dbPort = "";
+    $dbUser = "";
+    $dbPassword = "";
 
-    $dsn = 'mysql:host=//$OPENSHIFT_MYSQL_DB_HOST;dbname=cs313';
-    $username = 'adminlTPsP4X';
-    $password = 'lNuWHc1u6wUc';
+     $dbName = "cs313";
+
+     $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+
+     if ($openShiftVar === null || $openShiftVar == "")
+     {
+          // Not in the openshift environment
+          //echo "Using local credentials: "; 
+          require("setLocalDatabaseCredentials.php");
+     }
+     else 
+     { 
+          // In the openshift environment
+          //echo "Using openshift credentials: ";
+
+          $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+          $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+          $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+          $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+     } 
+     //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+
+     $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+     return $db;
+
+
 
     /**
      * Localhost connection settings
@@ -19,11 +43,11 @@
 //    $username = 'hartscre_admin';
 //    $password = 'Simalaya1';
 
-    try {
-        $db = new PDO($dsn, $username, $password);
-        //echo "Connected successfully"; 
-    } catch (PDOException $e) {
-        echo $error_message = $e->getMessage();
-        include('database_error.php');
-    }
+//    try {
+//        $db = new PDO($dsn, $username, $password);
+//        //echo "Connected successfully"; 
+//    } catch (PDOException $e) {
+//        echo $error_message = $e->getMessage();
+//        include('database_error.php');
+//    }
 ?>
